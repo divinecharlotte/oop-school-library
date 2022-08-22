df7612fd340467a3f024d6c8c014524a1abb0872
divine <divinemaina@gmail.com>
2022-08-22 20:24:35 +0200

    add configuration

---
+++ person.rb
@@ -0,0 +1,25 @@
+# class called person
+class Person
+    attr_reader :id
+    attr_accessor :name, :age
+
+    # initializing the class person 
+    def initialize(name = "Unknown", age,  parent_permission = true)
+        @id = rand(1..1000)
+        @name = name
+        @age = age
+    end
+    
+    # Private method is_of_age?
+    def is_of_age?
+        @age >= 18
+    end
+    private :is_of_age?
+
+    # Public method can_use_services?
+    def can_use_services?
+        is_of_age? || @parent_permission
+    end
+    public :can_use_services?
+
+end
