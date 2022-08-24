11a2d2da2208a776574554d1f5fc3a888498d3c4
divine <divinemaina@gmail.com>
2022-08-24 16:47:07 +0200

    Create the has-many side of Book and Rental (a book has many rentals).

--- book.rb
+++ book.rb
@@ -1,7 +1,11 @@
 class Book
-    attr_accessor : title, :author
+    attr_accessor : title, :author, :rentals
     def initialize(title, author)
         @title = title
         @author = author
+        @rentals = []
     end
+    def add_rental(date, person)
+        Rental.new(date, self, person)
+      end
 end
