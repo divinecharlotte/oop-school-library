f825c935e49d117e0007399fc2acde7c37567091
divine <divinemaina@gmail.com>
2022-08-25 16:32:16 +0200

    add create person  method

--- app.rb
+++ app.rb
@@ -18,3 +18,26 @@ puts 'there is no person'
        people.each{|person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
 end 
 
+def create_person(person)
+    puts 'do you want to create a student(1) or a teacher (2):'
+    print 'age:'
+    age = gets.chomp.to_i
+    print 'name:'
+    name = gets.chomp.to_i
+    
+    
+    case selected_person
+    when 1
+      print 'Has parent permission? [Y/N]: '
+      provided_permission = gets.chomp.capitalize
+      student_permission = true if provided_permission == 'Y'
+      student_permission = false if provided_permission == 'N'
+      person.push(Student.new(nil, age, name, parent_permission: student_permission))
+    when 2
+      print 'Specialization: '
+      specialization = gets.chomp
+      person.push(Teacher.new(specialization, age, name))
+    end
+    puts 'Person created successfully.'
+    end
+  
