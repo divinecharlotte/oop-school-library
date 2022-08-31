3ec4ae88877ffc68c602961574fed9b216207e1e
divine <divinemaina@gmail.com>
2022-08-31 17:48:08 +0200

    add rspec testing for book and all decolator files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require_relative '../book'
+require_relative '../student'
+require_relative '../classroom'
+require_relative '../teacher'
+require_relative '../person'
+require_relative '../rental'
+require_relative '../capitalize_decolator'
+require_relative '../trim_decolator'
